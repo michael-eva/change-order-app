c1306b822929967a389039cd460e428ad40df19d
galactic-shark <evamichael100@gmail.com>
2023-08-03 09:12:25 +0800

    dfgh

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
         <div>
             <NavBar />
             <ChangeOrderForm />
-            {/* <SignUp /> */}
+            <SignUp />
         </div>
     )
 }
