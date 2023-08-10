481a5b609fd49a5a800f1eae8145af57edd5829f
galactic-shark <evamichael100@gmail.com>
2023-08-11 06:23:06 +0800

    Added Links to nav components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,23 +9,13 @@ export default function NavBar() {
         <div className="nav">
             <img src="#" alt="Logo" />
             <Nav>
-                {/* <Link to="/"><Nav.Item>Home</Nav.Item></Link>
+                <Link to="/"><Nav.Item>Home</Nav.Item></Link>
                 <Link to="/order-history"><Nav.Item>Order History</Nav.Item></Link>
-                <Nav.Btn>Client Portal</Nav.Btn>
-                <Nav.Dropdown>
-                    <Nav.DropdownItem>Login</Nav.DropdownItem>
-                    <Link to="/signup"><Nav.DropdownItem>Signup</Nav.DropdownItem></Link>
-                </Nav.Dropdown> */}
-
-                <Nav.Item>Home</Nav.Item>
-                <Nav.Item>Order History</Nav.Item>
                 <Nav.Item>
                     <Nav.Button>Client Portal</Nav.Button>
-                    {/* nav-dropdown */}
                     <Nav.Dropdown>
-                        {/* nav-dropdown-item */}
                         <Nav.DropdownItem>Login</Nav.DropdownItem>
-                        <Nav.DropdownItem>Signup</Nav.DropdownItem>
+                        <Link to="/signup"><Nav.DropdownItem>Signup</Nav.DropdownItem></Link>
                     </Nav.Dropdown>
                 </Nav.Item>
             </Nav>
