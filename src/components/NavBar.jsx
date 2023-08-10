c929fa79c74f111188903efaf7c404ec09e74d39
galactic-shark <evamichael100@gmail.com>
2023-08-11 06:20:11 +0800

    Added a headless "Toggle" component to the NavBar. Restyled the nav dropdown - not finished yet, just palatable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import Nav from "../NavMenu/index"
-import Toggle from "../Toggle/index"
+
 
 
 export default function NavBar() {
@@ -9,14 +9,25 @@ export default function NavBar() {
         <div className="nav">
             <img src="#" alt="Logo" />
             <Nav>
-                <Link to="/"><Nav.Item>Home</Nav.Item></Link>
+                {/* <Link to="/"><Nav.Item>Home</Nav.Item></Link>
                 <Link to="/order-history"><Nav.Item>Order History</Nav.Item></Link>
                 <Nav.Btn>Client Portal</Nav.Btn>
-                {/* <Nav.Btn>Client Portal</Nav.Btn> */}
                 <Nav.Dropdown>
                     <Nav.DropdownItem>Login</Nav.DropdownItem>
                     <Link to="/signup"><Nav.DropdownItem>Signup</Nav.DropdownItem></Link>
-                </Nav.Dropdown>
+                </Nav.Dropdown> */}
+
+                <Nav.Item>Home</Nav.Item>
+                <Nav.Item>Order History</Nav.Item>
+                <Nav.Item>
+                    <Nav.Button>Client Portal</Nav.Button>
+                    {/* nav-dropdown */}
+                    <Nav.Dropdown>
+                        {/* nav-dropdown-item */}
+                        <Nav.DropdownItem>Login</Nav.DropdownItem>
+                        <Nav.DropdownItem>Signup</Nav.DropdownItem>
+                    </Nav.Dropdown>
+                </Nav.Item>
             </Nav>
         </div >
     )
