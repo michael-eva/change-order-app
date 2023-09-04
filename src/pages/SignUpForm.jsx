ae5715c813d7f55c941636ceb78522d4e8eec468
galactic-shark <evamichael100@gmail.com>
2023-09-04 15:25:38 +0800

    syncing with netlify

--- src/pages/SignUpForm.jsx
+++ src/pages/SignUpForm.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import supabase from "../config/supabaseClient";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 export default function SignUpForm({ session }) {
     const [companyName, setCompanyName] = useState('')
