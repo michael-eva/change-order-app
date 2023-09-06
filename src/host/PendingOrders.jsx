fcf7f27f49fa600adbecac909fda08e16c630a22
galactic-shark <evamichael100@gmail.com>
2023-09-06 20:08:43 +0800

    float order status change functionality

--- src/host/PendingOrders.jsx
+++ src/host/PendingOrders.jsx
@@ -7,51 +7,53 @@ import { formattedDate } from "../utils/dateUtils";
 import Toggle from "../Toggle";
 import FloatOrder from "./FloatOrder";
 import FloatOrderHistory from "./FloatOrderHistory";
+import { fetchPendingOrderData, fetchClientData, fetchFloatOrderData } from "../pages/FetchData";
 
 
 export default function PendingOrders({ session }) {
     const [searchParams, setSearchParams] = useSearchParams();
     const [selectedDay, setSelectedDay] = useState(formattedDate(new Date()));
-    const [data, setData] = useState([])
+    const [pendingOrders, setPendingOrders] = useState([])
+    const [pendingErrors, setPendingErrors] = useState('')
     const [floatOrder, setFloatOrder] = useState([])
     const [clientData, setClientData] = useState([])
-
-
-    useEffect(() => {
-        fetchData();
-        fetchClientData()
-        fetchFloatOrderData()
-    }, []);
-    const fetchData = async () => {
-        const { data } = await supabase
-            .from("change_order")
-            .select("*")
-            .eq('status', 'pending')
-
-        setData(data);
-    };
-    const fetchClientData = async () => {
-        const { data } = await supabase
-            .from("clients")
-            .select("*");
-
-        setClientData(data);
-    };
-    const fetchFloatOrderData = async () => {
-        try {
-            const { data, error } = await supabase
-                .from('float_order')
-                .select('*')
-                .eq('status', 'pending')
-            console.log(error);
-            setFloatOrder(data)
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    const [floatError, setFloatError] = useState('')
+    const [clientError, setClientError] = useState("")
+    const [floatOrderStatus, setFloatOrderStatus] = useState('')
 
     const dateFilter = searchParams.get("date");
 
+    useEffect(() => {
+        loadPendingOrders();
+        loadClientData()
+        loadFloatOrders()
+    }, []);
+    async function loadPendingOrders() {
+        try {
+            const data = await fetchPendingOrderData()
+            setPendingOrders(data)
+        } catch (error) {
+            setPendingErrors(error)
+        }
+    }
+    async function loadClientData() {
+        try {
+            const data = await fetchClientData()
+            setClientData(data)
+        } catch (error) {
+            setClientError(error)
+        }
+    }
+    async function loadFloatOrders() {
+        try {
+            const data = await fetchFloatOrderData()
+            setFloatOrder(data)
+        } catch (error) {
+            setFloatError(error)
+        }
+    }
+
+    // ORDER HISTORY
     const updateOrderStatus = async (orderId, newStatus) => {
         try {
             await supabase
@@ -62,32 +64,62 @@ export default function PendingOrders({ session }) {
             console.log(`Order ${orderId} status updated to ${newStatus}`);
 
             // Re-fetch the data to reflect the updated status
-            await fetchData();
+            await loadPendingOrders();
         } catch (error) {
             console.error(`Error updating order ${orderId} status:`, error);
         }
     };
+
+    // WEEK SLIDER
     function clickHandle(day) {
         setSelectedDay(formattedDate(day));
         setSearchParams(`?date=${formattedDate(day)}`)
     }
-    function buttonClick() {
 
+
+    // FLOAT ORDER HISTORY
+    function handleFloatStatusChange(newStatus) {
+        setFloatOrderStatus(newStatus)
     }
 
+    async function updateFloatOrderStatus(orderId, newFloatStatus) {
+        try {
+            await supabase
+                .from('float_order')
+                .update({ status: newFloatStatus })
+                .eq('id', orderId)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // HANDLE SUBMIT
+
+    async function handleSubmit() {
+        try {
+            for (const orderId in floatOrderStatus) {
+                const newFloatStatus = floatOrderStatus[orderId];
+                await updateFloatOrderStatus(orderId, newFloatStatus)
+            }
+            // console.log(newFloatStatus);
+            console.log("updating status");
+        } catch (error) {
+            console.log("error:", error);
+        }
+    }
     return (
         <>
-            {/* <button>Manually Add Change Order</button> */}
+            <button>Manually Add Change Order</button>
             <Toggle>
                 <Toggle.Button><button>Add Float Order</button></Toggle.Button>
                 <Toggle.On>
                     <FloatOrder session={session} />
                 </Toggle.On>
             </Toggle>
-            <WeekSlider clickHandle={clickHandle} selectedDay={selectedDay} data={data} floatOrder={floatOrder} />
-            <OrderHistory data={data} updateOrderStatus={updateOrderStatus} selectedDay={selectedDay} clientData={clientData} session={session} />
-            <FloatOrderHistory dateFilter={dateFilter} selectedDay={selectedDay} floatOrder={floatOrder} />
-            <button onClick={buttonClick}>Button</button>
+            <WeekSlider clickHandle={clickHandle} selectedDay={selectedDay} pendingOrders={pendingOrders} floatOrder={floatOrder} />
+            <OrderHistory pendingOrders={pendingOrders} updateOrderStatus={updateOrderStatus} selectedDay={selectedDay} clientData={clientData} session={session} />
+            <FloatOrderHistory dateFilter={dateFilter} selectedDay={selectedDay} floatOrder={floatOrder} handleFloatStatusChange={handleFloatStatusChange} />
+            <button onClick={handleSubmit}>Button</button>
         </>
     )
 }
