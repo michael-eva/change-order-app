bf1033532abfa484a127ecaf868cf7c0cfdaf97a
galactic-shark <evamichael100@gmail.com>
2023-09-20 07:51:54 +0800

    secured db keys

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wsolkhiobftucjmfkwkk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indzb2xraGlvYmZ0dWNqbWZrd2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE0ODU0MTAsImV4cCI6MjAwNzA2MTQxMH0.j0Xh-Xjni3I-7kD42LbQYBEqYm0IgagYEgBzKRa32bY'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(
     supabaseUrl,
     supabaseKey
