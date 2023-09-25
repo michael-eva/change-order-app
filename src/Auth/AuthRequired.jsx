97f0c9b0aa1b3ef2f5a61ac2dd4627084b3e7c49
galactic-shark <evamichael100@gmail.com>
2023-09-25 17:09:50 +0800

    syncing with netlify

--- src/Auth/AuthRequired.jsx
+++ src/Auth/AuthRequired.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export default function AuthRequired({ session }) {
     console.log(session?.user.role);
-    if (session?.user.role != "authenticated") {
+    if (session?.user.role !== "authenticated") {
         return <Navigate
             to={"/login"}
             state={{ message: "Please log in to continue" }}
